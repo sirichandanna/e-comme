96f9b771889e08d39156bfccd3f380d92c022f7d
siric <sirichandanagurram2004@gmail.com>
2024-12-30 11:10:14 +0530

    commit

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -7,7 +7,7 @@ const ListProduct = () => {
   const [allproducts,setAllProducts] = useState([]);
 
   const fetchInfo = async ()=>{
-    await fetch('http://localhost:4000/allproducts').then((res)=>res.json()).then((data)=>{setAllProducts(data)});
+    await fetch('https://e-comme-backende.onrender.com/allproducts').then((res)=>res.json()).then((data)=>{setAllProducts(data)});
   }
 
   useEffect(()=>{
@@ -15,7 +15,7 @@ const ListProduct = () => {
   },[])
 
    const remove_product = async(id)=>{
-    await fetch('http://localhost:4000/removeproduct',{
+    await fetch('https://e-comme-backende.onrender.com/removeproduct',{
       method:'POST',
       headers:{
         Accept:'application/json',
